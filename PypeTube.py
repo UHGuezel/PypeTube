924b5f39dc6716dea80185559b2ce912190e7520
Hasan Güzel <67553466+UHGuezel@users.noreply.github.com>
2023-05-30 14:00:13 +0200

    add autocreation and check for existance of dest

--- PypeTube.py
+++ PypeTube.py
@@ -1,9 +1,15 @@
-
+import os
 from pytube import YouTube
 
 PATH_SOURCE = "toDownload.txt"
+DEST_PATH = "Downloads"
 CONTENT_SOURCE = open(PATH_SOURCE, 'r').readlines()
 MAX_LINES = len(CONTENT_SOURCE)
+
+if os.path.exists(DEST_PATH):
+    print("Skip creation of Dest-File: File already exists.")
+else:
+    os.makedirs(DEST_PATH)
 
 try:
     for count, link in enumerate(CONTENT_SOURCE):
